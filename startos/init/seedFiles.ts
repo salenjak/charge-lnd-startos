c992f5c4977db90333ae27b464a23b3d0bb888af
salenjak <smolavi@protonmail.com>
2026-05-28 11:24:18 +0200

    Prevent seedFiles from overwriting user timer settings on reboot.

--- startos/init/seedFiles.ts
+++ startos/init/seedFiles.ts
@@ -3,10 +3,13 @@ import { settingsJson } from '../fileModels/settings.json'
 import { sdk } from '../sdk'
 import { defaultConfig } from '../utils'
 
-export const seedFiles = sdk.setupOnInit(async (effects) => {
+export const seedFiles = sdk.setupOnInit(async (effects, kind) => {
   const currentConfig = await chargeConfig.read().once()
   if (!currentConfig) {
     await chargeConfig.write(effects, defaultConfig)
   }
+  const currentSettings = await settingsJson.read().once()
+  if (!currentSettings) {
     await settingsJson.merge(effects, { intervalSeconds: 3600 })
+  }
 })
